68fb6a0545345abff22e779bd6798a5466288f5c
Rafeh Qazi <qazi@cleverprogrammer.com>
2017-05-05 17:24:38 -0500

    Add form and edit view.

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
 from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect
+from .form import PostForm
 
 
 def post_list(request):
@@ -12,4 +13,17 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
-    pass
+
+
+def post_new(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
