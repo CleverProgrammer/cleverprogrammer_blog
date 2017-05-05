74df48d8a87d9732c2f98cc72203e77277e44085
Rafeh Qazi <qazi@cleverprogrammer.com>
2017-05-05 18:22:06 -0500

    Add post_edit view, and url.

--- blog/views.py
+++ blog/views.py
@@ -27,3 +27,18 @@ def post_new(request):
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
