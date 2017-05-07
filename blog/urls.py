fbd35c8b68ca68bde0ef241b50c7b0bb1eaf3ed8
Rafeh Qazi <qazi@cleverprogrammer.com>
2017-05-06 23:05:24 -0500

    Add notification badge and read notifications.

--- blog/urls.py
+++ blog/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     url(r'^post/(?P<pk>\d+)/publish/$', views.post_publish, name='post_publish'),
     url(r'^post/(?P<pk>\d+)/comment/$', views.add_comment_to_post, name='add_comment_to_post'),
     url(r'^comment/(?P<pk>\d+)/approve/$', views.comment_approve, name='comment_approve'),
-    url(r'^comment/(?P<pk>\d+)/remove/$', views.comment_remove, name='comment_remove')
+    url(r'^comment/(?P<pk>\d+)/remove/$', views.comment_remove, name='comment_remove'),
+    url(r'^my_unreads/$', views.my_unread_notifications, name='my_unread_notifications'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,11 +3,17 @@ from django.utils import timezone
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post, Comment
 from .form import PostForm, CommentForm
+from notifications.views import AllNotificationsList, UnreadNotificationsList
 
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def my_unread_notifications(request):
+    my_unreads = UnreadNotificationsList
+    return render(request, 'blog/unread_notifications.html', {'my_unreads': my_unreads})
 
 
 @login_required
