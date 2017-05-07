073aeb0a934477d3dde73d10bb779ae6683296a4
Rafeh Qazi <qazi@cleverprogrammer.com>
2017-05-07 01:45:20 -0500

    Add login required to my_unread_notifications view.

--- blog/views.py
+++ blog/views.py
@@ -11,6 +11,7 @@ def post_list(request):
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 
+@login_required
 def my_unread_notifications(request):
     my_unreads = request.user.notifications.unread()
     # live_unreads = live_unread_notification_list(request)
