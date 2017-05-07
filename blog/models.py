34e56a6a6dc46fd3e7280ba5dd00b9532e1898ec
Rafeh Qazi <qazi@cleverprogrammer.com>
2017-05-06 19:34:39 -0500

    count only approved comments.

--- blog/models.py
+++ blog/models.py
@@ -14,6 +14,9 @@ class Post(models.Model):
         self.published_date = timezone.now()
         self.save()
 
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
+
     def __str__(self):
         return self.title
 
