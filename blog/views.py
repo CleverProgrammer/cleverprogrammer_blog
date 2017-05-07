3845de18a05ccbddc5295df9a180bf1a74ae91fa
Rafeh Qazi <qazi@cleverprogrammer.com>
2017-05-06 20:08:02 -0500

    Posts now have markdown as well.

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,7 @@ from .form import PostForm, CommentForm
 
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 
