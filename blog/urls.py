018c9f63e9ca04ef6769e58bda2eb473db8cfdd9
Rafeh Qazi <qazi@cleverprogrammer.com>
2018-07-24 23:50:46 -0700

    Modernize urls to paths.

--- blog/urls.py
+++ blog/urls.py
@@ -1,16 +1,16 @@
-from django.conf.urls import url
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
-    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
-    url(r'^post/new/$', views.post_new, name='post_new'),
-    url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
-    url(r'^drafts/$', views.post_draft_list, name='post_draft_list'),
-    url(r'^post/(?P<pk>\d+)/publish/$', views.post_publish, name='post_publish'),
-    url(r'^post/(?P<pk>\d+)/comment/$', views.add_comment_to_post, name='add_comment_to_post'),
-    url(r'^comment/(?P<pk>\d+)/approve/$', views.comment_approve, name='comment_approve'),
-    url(r'^comment/(?P<pk>\d+)/remove/$', views.comment_remove, name='comment_remove'),
-    url(r'^my_unreads/$', views.my_unread_notifications, name='my_unread_notifications'),
-    url(r'^signup/$', views.signup, name='signup'),
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('drafts/', views.post_draft_list, name='post_draft_list'),
+    path('post/<int:pk>/publish/', views.post_publish, name='post_publish'),
+    path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
+    path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
+    path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
+    path('my_unreads/', views.my_unread_notifications, name='my_unread_notifications'),
+    path('signup/', views.signup, name='signup'),
 ]
--- mysite/urls.py
+++ mysite/urls.py
@@ -5,26 +5,26 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+    2. Add a URL to urlpatterns:  path('$', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  path('$', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 import notifications.urls
-from django.conf.urls import include, url
+from django.urls import include, path
 from django.contrib import admin
 
 from django.contrib.auth.views import login
 from django.contrib.auth.views import logout
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(regex=r'^accounts/login/$', view=login, kwargs={'template_name': 'login.html'}, name='login'),
-    url(regex=r'^accounts/logout/$', view=logout, kwargs={'template_name': 'logout.html', 'next_page': '/'},
+    path('admin/', admin.site.urls),
+    path('accounts/login/', view=login, kwargs={'template_name': 'login.html'}, name='login'),
+    path('accounts/logout/', view=logout, kwargs={'template_name': 'logout.html', 'next_page': '/'},
         name='logout'),
-    url(r'', include('blog.urls')),
-    url(r'^inbox/notifications/', include(notifications.urls, namespace='notifications')),
+    path('', include('blog.urls')),
+    path('inbox/notifications/', include(notifications.urls, namespace='notifications')),
 ]
